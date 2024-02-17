c57c023687e230728ce9c033899a1df27c831dee
Chengbai <1961794290@qq.com>
2024-02-17 22:35:33 +0800

    chore:完善了项目配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-
+import Elementplus from 'element-plus'
+//@ts-expect-error 是js文件没有ts类型所以忽略下面错误
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
 
-const fun = () => {
-  console.log('我爱祖国')
-}
-fun()
+app.use(Elementplus, {
+  locale: zhCn,
+})
+
+app.mount('#app')
